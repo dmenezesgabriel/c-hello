85ec3bfe1a865c78bbd302b3da5f33eb850b2b60
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-22 19:01:34 -0300

    Update hello

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,26 @@
+/*
+Import libs
+The "Include" statement is a directive from the compiler and not a language
+command
+*/
 #include <stdio.h>
 #include <locale.h>
 
 int main() {
+    int x;
+    float y;
+    char z;
+    x = 1;
+    y = 2.3;
+    z = 'A';
+
     setlocale(LC_ALL, "");
     printf("Hello, World!");
+
+    printf("\nx value is: %d", x);
+    printf("\ny value is: %f", y);
+    printf("\nz value is: %c", z);
+
+    y = 8.5421;
+    printf("\nx plus 1 = %d and z still = %c", x + 1, z);
 }
